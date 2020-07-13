9dcd0b963f60556e3f781bdc452748711588e1c2
Anil kumar <anil.k@greyorange.com>
2020-07-14 01:17:56 +0530

    Updates

---
+++ precache-manifest.96b751da6004292e4ae0e5b9f77eff2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2f6097d5646a10b862f47004d8c058",
+    "url": "/anil-pace/react-toDoApp/index.html"
+  },
+  {
+    "revision": "2fd96dd511b9c85ada11",
+    "url": "/anil-pace/react-toDoApp/static/css/main.44e09bb4.chunk.css"
+  },
+  {
+    "revision": "58e7156088ef2be2caa9",
+    "url": "/anil-pace/react-toDoApp/static/js/2.5fc9803f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anil-pace/react-toDoApp/static/js/2.5fc9803f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd96dd511b9c85ada11",
+    "url": "/anil-pace/react-toDoApp/static/js/main.dc714ae8.chunk.js"
+  },
+  {
+    "revision": "0903cd812d7934b87309",
+    "url": "/anil-pace/react-toDoApp/static/js/runtime-main.55f63da6.js"
+  }
+]);
