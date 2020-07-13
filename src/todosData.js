b1c95d7d0c684279335e479a09c7e3b1b2f24744
Anil kumar <anil.k@greyorange.com>
2020-07-14 00:47:31 +0530

    BSS-12345: initial commit

---
+++ src/todosData.js
@@ -0,0 +1,24 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Thank GOD for a beautiful day, ahead !!!",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Build a ToDo App using React",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "Call Family members",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Thank GOD for a beautiful day, today",
+        completed: true
+    }
+]
+
+export default todosData
